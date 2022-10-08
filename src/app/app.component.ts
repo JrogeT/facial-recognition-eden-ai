e513e1a9cbf137390f2637fb6eb055fa9cdf655e
JrogeT <jorgerodrigotorrez@gmail.com>
2022-10-07 21:37:17 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'facial-recogn-ia';
+
+  file?: File | null;
+  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMWUxOTU1OTgtY2ZlYi00MDAyLWI0OTItOTAxNjU0ZDFlNWQxIiwidHlwZSI6ImFwaV90b2tlbiJ9.aXlgJSLGFdWx78Uqu8KrtsH7zfcyGM4jl7gOsBGVh9Q';
+
+  constructor(
+    private httpClient: HttpClient
+  ) {  }
+
+  fileChange(files: any) {
+    this.file = files.target.files.item(0);
+  }
+
+  uploadFile() {
+    if(!this.file) {
+      alert('Please select a file');
+      return;
+    }
+
+    const headers = {
+      'Authorization': `Bearer ${this.token}`,
+    };
+
+    const formData = new FormData();
+    formData.append('providers', 'amazon');
+    formData.append('file', this.file!);
+
+    this.httpClient.post<any>(
+      'https://api.edenai.run/v2/image/face_detection',
+      formData,
+      {headers},
+    ).subscribe((response: any) => {
+        console.log(response.amazon.items[0].landmarks.left_eye);
+    });
+  }
+
 }
